f5ab287b5c3db864f5f555323a08e3af303ab003
Scott Murphy <scottmurphy1111@gmail.com>
2023-10-11 10:23:26 -0400

    init, first pass

---
+++ src/lib/components/LabelInput.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	export let label: string;
+	export let placeholder: string;
+	export let name: string;
+	export let value: number;
+</script>
+
+<label class="flex flex-col gap-1 font-semibold text-base"
+	>{label}
+	<input
+		type="number"
+		class="border border-ts-gray-md rounded py-3 px-4"
+		{name}
+		{placeholder}
+		bind:value
+	/>
+</label>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+	import logo from '$lib/assets/images/ts-logo.svg';
+	import JustDriveIcon from '$lib/assets/icons/just-drive.svelte';
+
+	import { writable } from 'svelte/store';
+
+	const price = writable(1000);
+	const term = writable(12);
+	const termOptions = [12, 24, 36, 48];
+	const fee = 1.21999;
+	let weeklyTotal = '';
+
+	$: weeklyTotal = (($price * fee) / ($term - 2) / 4).toFixed(2);
+</script>
+
+<div class="w-full h-screen gap-12 flex flex-col justify-center items-center">
+	<h2 class="text-xl">Estimated Weekly Payment Calculator</h2>
+	<div class="border-2 border-ts-gray-md p-8 rounded-xl shadow-md">
+		<a class="block mb-12" href="/">
+			<img class="block w-full" src={logo} alt="ts-logo" />
+		</a>
+		<div class="flex flex-col gap-8">
+			<label class="flex flex-col gap-2 font-semibold justify-center text-base"
+				>Total Base Amount
+				<input
+					type="number"
+					step="100"
+					class="border border-ts-gray-md rounded py-3 px-4"
+					bind:value={$price}
+				/>
+			</label>
+
+			<label class="flex flex-col gap-2 font-semibold justify-center text-base"
+				>Term Length
+				<select class="border border-ts-gray-md rounded py-3 px-4" bind:value={$term}>
+					{#each termOptions as option}
+						<option value={option}>{option} Months</option>
+					{/each}
+				</select>
+			</label>
+			<div>
+				<span class="font-semibold mb-2 flex">Your estimated weekly payment amount is:</span>
+				<div class="w-full flex flex-col font-semibold mb-4">
+					<span class="w-full inline-flex justify-end border-b-2 border-black text-right ml-auto">
+						${weeklyTotal}*
+					</span>
+				</div>
+				<span
+					class="font-medium flex w-[325px] text-ts-blue text-xs italic leading-tight tracking-tight antialiased"
+					>* This is an estimate. Final payment amount subject to credit approval and may differ
+					from above calculation.</span
+				>
+			</div>
+		</div>
+		<div class="w-full mt-8 flex justify-center items-center">
+			<div class="w-1/2">
+				<JustDriveIcon />
+			</div>
+		</div>
+	</div>
+</div>
